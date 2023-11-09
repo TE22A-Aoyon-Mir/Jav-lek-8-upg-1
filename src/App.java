9e13cf330f5b82707dd61314d05ad5551f1836ed
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-11-09 11:08:10 +0100

    Gjort klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner t = new Scanner(System.in);
+
+        //a
+        String str = "Yes";
+        while (str.equalsIgnoreCase("Yes"))
+        {
+            System.out.println("0 1 2 3 4 5");
+        }
     }
 }
