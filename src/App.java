5ca86c1b272d63e9336f6f3042fbb6004c178058
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-11-15 10:06:52 +0100

    Gjort klar del f

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ public class App {
         Scanner tb = new Scanner(System.in);
 
         //a & b
+        System.out.println("\n");
         int varv = 2;
         while(varv<6)
         {
@@ -14,12 +15,14 @@ public class App {
         }
 
         //c
+        System.out.println("\n");
         for(int varv1=0; varv1<6; varv1++)
         {
             System.out.println("Varv1 nr: "+varv1);
         }
 
         //d
+        System.out.println("\n");
         int varv2 = 0;
         do
         {
@@ -29,11 +32,21 @@ public class App {
         while(varv2<6);
 
         //e
+        System.out.println("\n");
         int varv3 = 5;
         while(varv3>0)
         {
             System.out.println("Varv3 nr: "+varv3);
             varv3--;
+        }
+
+        //f
+        System.out.println("\n");
+        {
+            for(int varv4 = 5; varv4 >-1; varv4--)
+            {
+                System.out.println("Varv4 nr: "+varv4);
+            }
         }
     }
 }
